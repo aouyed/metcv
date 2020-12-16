f644e08eec0c5290ee486df6d035331306a1ed10
Ouyed Hernandez <amirouyed@has-predict.catnet.arizona.edu>
2020-12-15 18:37:41 -0700

    regridder again

--- data/raw/regridder.py
+++ data/raw/regridder.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import numpy as np
 import xesmf as xe
-
+from datetime import datetime
 
 ds = xr.open_dataset('MCS_Data4tracking_17_00.nc')
 latmax = ds['lat'].max().item()
@@ -14,5 +14,9 @@ new_lon = np.arange(lonmin, lonmax, 0.0267)
 
 ds_out = xr.Dataset({'lat': (['lat'], new_lat), 'lon': ('lon', new_lon), })
 regridder = xe.Regridder(ds, ds_out, 'bilinear')
-dr_out = regridder(ds['qv_nr'])
+dr_out = regridder(ds[['qv_nr', 'qv_cld']])
+date_unit = datetime(2009, 11, 4, 17, 0, 0, 0)
+dr_out = dr_out.expand_dims('time')
+date = np.array([date_unit])
+dr_out = dr_out.assign_coords(time=date)
 breakpoint()
