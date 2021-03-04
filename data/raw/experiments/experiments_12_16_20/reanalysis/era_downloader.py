b62e20e2fff41d31cdb3d1cb951bc603b8d9e244
Ouyed Hernandez <amirouyed@has-predict.catnet.arizona.edu>
2021-03-03 18:11:31 -0700

    added sorting

---
+++ data/raw/experiments/experiments_12_16_20/reanalysis/era_downloader.py
@@ -0,0 +1,33 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+#var = 'v'
+year = '2009'
+month = '11'
+#day = '04'
+#time = '17:00:00'
+pressures = ['800', '300']
+#timen = '12_00_00'
+
+days = ['04']
+times = ['17:00:00']
+vars = ['u', 'v']
+
+for pressure in pressures:
+    for day in days:
+        for time in times:
+            for var in vars:
+                c.retrieve(
+                    'reanalysis-era5-pressure-levels',
+                    {
+                        'product_type': 'reanalysis',
+                        'variable': var,
+                        'pressure_level': pressure,
+                        'year': year,
+                        'month': month,
+                        'day': day,
+                        'time': time,
+                        'format': 'netcdf',                 # Supported format: grib and netcdf. Default: grib
+                    },
+                    var + '_'+pressure + '_'+year + '_'+month+'_'+day + '_'+time + '_era5.nc')
---
+++ data/raw/sorter.py
@@ -0,0 +1,41 @@
+import datetime
+import os
+
+
+def daterange(start_date, end_date, dhour):
+    date_list = []
+    delta = datetime.timedelta(hours=dhour)
+    while start_date <= end_date:
+        date_list.append(start_date)
+        start_date += delta
+    return date_list
+
+
+month = 1
+day = 1
+datelists = {}
+for day in [1, 2, 3]:
+
+    d1 = datetime.datetime(2006, month, day, 19, 0, 0, 0)
+    d0 = d1 - datetime.timedelta(hours=20)
+    print('d0 ' + str(d0))
+    start_dates = daterange(d0, d1, 6)
+    end_dates = daterange(d0+datetime.timedelta(hours=2),
+                          d1+datetime.timedelta(hours=2), 6)
+
+    date_list = []
+    for i, start_date in enumerate(start_dates):
+        date_list = date_list + daterange(start_date, end_dates[i], 0.5)
+
+    datelists[day] = date_list
+
+
+for day in [2, 3, 4]:
+    dates = datelists[day]
+
+    for date in dates:
+        string_date = date.strftime("%Y%m%d_%H%M")
+        filename = 'NRData_300hPa_'+string_date+'.nc'
+        print(string_date)
+        os.system('mv NR300/'+filename +
+                  ' ../raw/experiments/jpl/january/0'+str(day)+'/'+filename)
